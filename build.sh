5b74adc09e847d135685e44e53cd9ab456253c52
swatikadivar <swka4307@colorado.edu>
2021-04-27 23:16:47 -0600

    adding WiFi and ssh

--- build.sh
+++ build.sh
@@ -17,8 +17,15 @@ IMAGE="IMAGE_FSTYPES = \"wic.bz2\""
 #Set GPU memory as minimum
 MEMORY="GPU_MEM = \"16\""
 
+#Add wifi support
+DISTRO_F="DISTRO_FEATURES_append = \"wifi\""
+#add firmware support 
+IMAGE_ADD="IMAGE_INSTALL_append = \"linux-firmware-rpidistro-bcm43430 v4l-utils python3 ntp wpa-supplicant\""
+
 #Licence
 LICENCE="LICENSE_FLAGS_WHITELIST = \"commercial\""
+
+IMAGE_F="IMAGE_FEATURES += \"ssh-server-openssh\""
 
 cat conf/local.conf | grep "${CONFLINE}" > /dev/null
 local_conf_info=$?
@@ -29,8 +36,18 @@ local_image_info=$?
 cat conf/local.conf | grep "${MEMORY}" > /dev/null
 local_memory_info=$?
 
+cat conf/local.conf | grep "${DISTRO_F}" > /dev/null
+local_distro_info=$?
+
+cat conf/local.conf | grep "${IMAGE_ADD}" > /dev/null
+local_imgadd_info=$?
+
 cat conf/local.conf | grep "${LICENCE}" > /dev/null
 local_licn_info=$?
+
+cat conf/local.conf | grep "${IMAGE_F}" > /dev/null
+local_imgf_info=$?
+
 
 
 if [ $local_conf_info -ne 0 ];then
@@ -55,6 +72,20 @@ else
 	echo "${MEMORY} already exists in the local.conf file"
 fi
 
+if [ $local_distro_info -ne 0 ];then
+    echo "Append ${DISTRO_F} in the local.conf file"
+	echo ${DISTRO_F} >> conf/local.conf
+else
+	echo "${DISTRO_F} already exists in the local.conf file"
+fi
+
+if [ $local_imgadd_info -ne 0 ];then
+    echo "Append ${IMAGE_ADD} in the local.conf file"
+	echo ${IMAGE_ADD} >> conf/local.conf
+else
+	echo "${IMAGE_ADD} already exists in the local.conf file"
+fi
+
 if [ $local_licn_info -ne 0 ];then
     echo "Append ${LICENCE} in the local.conf file"
 	echo ${LICENCE} >> conf/local.conf
@@ -62,9 +93,50 @@ else
 	echo "${LICENCE} already exists in the local.conf file"
 fi
 
+if [ $local_imgf_info -ne 0 ];then
+    echo "Append ${IMAGE_F} in the local.conf file"
+	echo ${IMAGE_F} >> conf/local.conf
+else
+	echo "${IMAGE_F} already exists in the local.conf file"
+fi
+
+
+
 
 bitbake-layers show-layers | grep "meta-raspberrypi" > /dev/null
 layer_info=$?
+
+bitbake-layers show-layers | grep "meta-python" > /dev/null
+layer_python_info=$?
+
+bitbake-layers show-layers | grep "meta-oe" > /dev/null
+layer_metaoe_info=$?
+
+bitbake-layers show-layers | grep "meta-networking" > /dev/null
+layer_networking_info=$?
+
+if [ $layer_metaoe_info -ne 0 ];then
+    echo "Adding meta-oe layer"
+	bitbake-layers add-layer ../meta-openembedded/meta-oe
+else
+	echo "layer meta-oe already exists"
+fi
+
+
+if [ $layer_python_info -ne 0 ];then
+    echo "Adding meta-python layer"
+	bitbake-layers add-layer ../meta-openembedded/meta-python
+else
+	echo "layer meta-python already exists"
+fi
+
+
+if [ $layer_networking_info -ne 0 ];then
+    echo "Adding meta-networking layer"
+	bitbake-layers add-layer ../meta-openembedded/meta-networking
+else
+	echo "layer meta-networking already exists"
+fi
 
 
 if [ $layer_info -ne 0 ];then
